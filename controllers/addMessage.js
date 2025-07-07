4d721755ae722e38c5a74ae8711c910ca994f5d6
Artem <161304626+RTJIL@users.noreply.github.com>
2025-07-07 15:38:03 +0000

    store data in json + open specific messages

--- controllers/addMessage.js
+++ controllers/addMessage.js
@@ -1,10 +1,12 @@
 import { messages, saveMessages } from "../data/data.js"
 import formatDate from "../utils/formatDate.js"
+import { v4 as uuidv4 } from "uuid"
 
 export const addMessage = (req, res) => {
   const { user, message } = req.body
 
   messages.push({
+    id: uuidv4(),
     text: message,
     user,
     added: formatDate(new Date()),
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,10 +2,11 @@ import { Router } from "express"
 import { getAllMessages } from "../controllers/getAllMessages.js"
 import { getForm } from "../controllers/getForm.js"
 import { addMessage } from "../controllers/addMessage.js"
+import { getMessageById } from "../controllers/getMessageById.js"
 
 export const indexRouter = Router()
 
 indexRouter.get("", getAllMessages)
 indexRouter.get("/new", getForm)
 indexRouter.post("/new", addMessage)
-// indexRouter.get(":id", getMessageById)
+indexRouter.get("/:messageId", getMessageById)
